d313cb82c008394035c4f1b30ecb123fe7b57926
coungard <coungard@mail.ru>
2018-09-25 16:43:48 +0300

    commit 4 into test

--- src/main/java/com/swing/ch08/HTMLabel.java
+++ src/main/java/com/swing/ch08/HTMLabel.java
@@ -6,7 +6,7 @@ public class HTMLabel extends JFrame {
     private String html1 = "<html><b>Слава</b><font size=5 color=red> HTML";
     private String html2 = "<html><font size=4 color=blue>" + "<ul>Список:<li>Один<li>Два";
     private String html3 = "<html><body bgcolor=orange><h2>Любой цвет фона";
-    private String html4 = "<html><img src=\"file:src/main/resources/monkey.gif\">";
+    private String html4 = "<html><img src=\"file:src/main/resources/images/monkey.gif\">";
     public HTMLabel() {
         super("HTMLabel");
         // при закрытии окна выход
