dcbd115ef30e1d9ef75331ce4f9d8f3d7963c744
coungard <coungard@mail.ru>
2018-09-25 16:09:51 +0300

    swing book, p.327

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,16 +1,12 @@
 package com;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test{
-    public static void main(String[] args) {
 
-        Map<String, Double> map = new HashMap<>();
-
-        map.put("pi", 3.14159);
-//        map.put("e", 2L);                         fail
-        map.put("log(1)", new Double(0.0));
-//        map.put('x', new Double(123.4));          fail
+    public static int testMethod(int count, int val) {
+        int res = 0;
+        for (int i = 0; i < count; i++) {
+            res += val;
+        }
+        return res;
     }
 }
